a04c398582b86ac794600e870efb030430c18460
Vivek Shukla <vivekshuklahere@gmail.com>
2024-08-11 15:09:23 +0530

    added variables

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -15,7 +15,7 @@ fn main() {
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-
+ 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
--- hello_world/main.rs
+++ hello_world/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("Hello, world arvin!");
+    println!("Hello, world!");
 }
