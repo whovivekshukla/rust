5a72ad2e47b3832c81e8d70c6b48c359c97a6590
Vivek Shukla <vivekshuklahere@gmail.com>
2024-08-11 16:36:07 +0530

    done with functions

---
+++ functions/src/main.rs
@@ -0,0 +1,40 @@
+// fn main() {
+//     print_labeled_measurement(5, 'h');
+// }
+
+// fn print_labeled_measurement(value: i32, unit_label: char) {
+//     println!("The measurement is: {value}{unit_label}");
+// }
+
+// fn main() {
+//     let y = {
+//         let x = 3;
+//         x + 1
+//     };
+
+//     println!("The value of y is: {y}");
+// }
+
+
+
+// fn five() -> i32 {
+//     5
+// }
+
+// fn main() {
+//     let x = five();
+
+//     println!("The value of x is: {x}");
+// }
+
+
+
+fn main() {
+    let x = plus_one(5);
+
+    println!("The value of x is: {x}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
