42246fc484fb57ad51c5b9f79d68ff1527ce9cff
Vivek Shukla <vivekshuklahere@gmail.com>
2024-08-11 15:21:13 +0530

    update

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -8,21 +8,39 @@
 // }
 
 
-fn main() {
-    let x = 5;
+// fn main() {
+//     let x = 5;
 
-    let x = x + 1;
+//     let x = x + 1;
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
+//     {
+//         let x = x * 2;
+//         println!("The value of x in the inner scope is: {x}");
+//     }
 
-    println!("The value of x is: {x}");
+//     println!("The value of x is: {x}");
 
-    let spaces = "    ";
-    let spaces = spaces.len();
+//     let spaces = "    ";
+//     let spaces = spaces.len();
 
 
-    println!("The value of spaces is: {spaces}");
-}
+//     println!("The value of spaces is: {spaces}");
+// }
+
+
+   // fn main() {
+   //  let guess: u32 = "42".parse().expect("Not a number");
+   //  println!("The guess number is {}", guess);
+   //  }   
+
+
+   fn main() {
+    let c = 'z';
+    let z: char = 'ℤ'; // with explicit type annotation
+    let heart_eyed_cat = '😻';
+
+    println!("the value of c is {}", c);
+    println!("the value of z is {}", z);
+    println!("the value of heart eyed cat is {}", heart_eyed_cat);
+
+    }   
